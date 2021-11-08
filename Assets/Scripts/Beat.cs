31f4c6079acd493608e06f234558472ee6a49ccc
Tal <talzhtc@gmail.com>
2021-11-08 19:08:07 +0200

    First commit

---
+++ Assets/Scripts/Beat.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beat : MonoBehaviour
+{
+    [SerializeField] float enlarge_by = 1f;
+    [SerializeField] float speed = 1f;
+    float init_scale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        init_scale = transform.localScale.x; // in our case, x = y = z
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = (float)Mathf.Abs(Mathf.Sin(speed*Time.time))*enlarge_by + init_scale;
+        transform.localScale = new Vector3(x, x, x);
+    }
+}
---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+
+    [SerializeField] private float max_distance = 1;
+    [SerializeField] private float speed = 1;
+    private float origin_x;
+    public enum directions
+    {
+        Right,
+        Left
+    };
+    public directions starting_direction;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        origin_x = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x;
+        if (starting_direction == directions.Left)
+        {
+            x = (origin_x - Mathf.Sin(speed*Time.time) * max_distance);
+        }
+        else
+        {
+            x = (origin_x + Mathf.Sin(speed*Time.time) * max_distance);
+        }
+        
+        float y = transform.position.y;
+        float z = transform.position.z;
+        Vector3 v = new Vector3(x, y, z);
+        transform.position = v;
+    }
+}
